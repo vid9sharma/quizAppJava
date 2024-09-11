efe9e4495288319cbd4e14525b769630ba9748e1
vid9sharma <vid9sharma@gmail.com>
2024-09-11 19:50:22 -0400

    validate jwt token

---
+++ src/main/java/com/example/quizAppJava/config/JwtFilter.java
@@ -0,0 +1,65 @@
+package com.example.quizAppJava.config;
+
+import com.example.quizAppJava.service.JWTService;
+import com.example.quizAppJava.service.MyUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JWTService jwtService;
+
+  @Autowired
+  private ApplicationContext context;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    // get header to fetch token and username
+    String authHeader = request.getHeader("Authorization");
+    String token = null;
+    String username = null;
+
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      // get token from header
+      token = authHeader.substring(7);
+
+      // extract username from token
+      try {
+        username = jwtService.extractUsername(token);
+      } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      // get user details
+      UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(username);
+
+      // validate jwt token
+      if (jwtService.validateToken(token, userDetails)) {
+        // get username-password token
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        // set authentication for the above token
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+    }
+  }
+}
--- src/main/java/com/example/quizAppJava/service/JWTService.java
+++ src/main/java/com/example/quizAppJava/service/JWTService.java
@@ -1,18 +1,19 @@
 package com.example.quizAppJava.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.beans.Encoder;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
@@ -25,16 +26,37 @@ public class JWTService {
         .add(claims)
         .subject(username)
         .issuedAt(new Date(System.currentTimeMillis()))
-        .expiration(new Date(System.currentTimeMillis() + 60*60*30))
+        .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
         .and()
         .signWith(getKey())
         .compact();
   }
 
-  public Key getKey() throws NoSuchAlgorithmException {
+  public SecretKey getKey() throws NoSuchAlgorithmException {
     KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
     SecretKey secretKey = keyGenerator.generateKey();
 
     return Keys.hmacShaKeyFor(secretKey.getEncoded());
+  }
+
+  public String extractUsername(String token) throws NoSuchAlgorithmException {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimResolver) throws NoSuchAlgorithmException {
+    final Claims claims = extractAllClaims(token);
+    return claimResolver.apply(claims);
+  }
+
+  public Claims extractAllClaims(String token) throws NoSuchAlgorithmException {
+    return Jwts.parser()
+        .verifyWith(getKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  public boolean validateToken(String token, UserDetails userDetails) {
+    return true;
   }
 }
